96d825fd06b24f52e000ab8fd6528b7097678e43
BikinMV <BikinMV@OSSEDO35>
2021-08-13 11:15:55 +0300

    Добавьте файлы проекта.

---
+++ LogCollectorConsole/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogCollectorConsole
+{
+    #region Ref
+
+    /*Cryptography 
+    + Referent.log; (C:\Dipost\log\Referent)
+    + ref_crypto_логин организации.log;(C:\Dipost\log\Referent)
+    + CPCrypto.ini; (C:\Dipost)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini (C:\Dipost)
+    */
+
+    /*Transport
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + Reftransport.log (C:\Dipost\log\Referent)
+    + Protokol_ логин организации.log; (C:\Dipost\log\\Referent)
+    + FormatCheck.log (C:\Dipost\log\FormatCheck) 
+    */
+
+    /*Update
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + TXUpdater_ggggmmnnhhmmss.log (C:\Dipost) 
+    */
+
+    /*TaxControl
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + FormatCheck.log (C:\Dipost\log\FormatCheck)
+    + Referent.log; (C:\Dipost\log\Referent) 
+    */
+
+    /*ImportInDB
+    * + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini (C:\Dipost) 
+     */
+
+    /* PostProcessing/Visualization
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + FormatCheck.log; (С:\Dipost\log\FormatCheck)
+    + DocEngineError.log; (C:\Dipost)
+    + CPCrypto.ini; (C:\Dipost)
+    + ref_crypto_логин организации.log;(C:\Dipost\log\Referent)​ 
+    */
+
+    /*DBUpdate
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + dover.fdb; (C:\Dipost\db)
+    + DB\Backup. (C:\Dipost\db\backup)    
+    */
+
+    /*DBBreak
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + DB\Referent.~fN; (C:\Dipost\db)
+    + DB\Backup. (C:\Dipost\db\backup)  
+    */
+
+    /*Connection
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)    
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini; (C:\Dipost)
+    + dbconnection.ini. (C:\Dipost) 
+    */
+
+    /*CirculationError
+    + Referent.log; (C:\Dipost\log\Referent)
+    + ref_crypto_логин организации.log (C:\Dipost\log\Referent)
+    + CPCrypto.ini; (C:\Dipost)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini (C:\Dipost)  
+    */
+
+    /*Other
+    + Referent.log; (C:\Dipost\log\Referent)
+    + Referent0.ini; (C:\Dipost)
+    + referent.ini; (C:\Dipost)
+    + Referent_Setup.ini. (C:\Dipost) 
+    */
+    #endregion
+
+    #region DoclinerSingle
+
+    /*LaunchUpdateError
+    +C:\Taxcom\Docliner Single\Client\log\ReferentNet.log 
+    +C:\Taxcom\DoclinerSingle\Server\RefNetUpdate\log\RefNetUpdate.log
+    +C:\Taxcom\Docliner Single\Client\log\Launcher.log
+    +C:\Taxcom\Docliner Single\Server\log\NativeUpdater\NativeUpdater.log
+    +C:\Taxcom\Docliner Single\Server\log\RefNetService\RefNetService.log 
+    */
+
+    /*SendRecieveError 
+    + C:\Taxcom\Docliner Single\Client\log\ReferentNet.log 
+    + C:\Taxcom\Docliner Single\Client\log\TISLoader\TISLoader.log
+    + C:\Taxcom\Docliner Single\Server\log\CPCrypto\CPCrypto.log
+    + C:\Taxcom\Docliner Single\Server\log\MailProcessor\MailProcessor.log
+    + C:\Taxcom\Docliner Single\Server\log\UnifiedFormat\UnifiedFormat.log 
+    */
+
+    /*Import
+    + C:\Taxcom\Docliner Single\Client\log\ReferentNet.log 
+    + C:\Taxcom\Docliner Single\Client\log\FormatCheck\FormatCheck.log     
+    */
+
+    /*DocCheck
+    +C:\Taxcom\Docliner\Server\log\FormatCheck\FormatCheck.log
+    + C:\Taxcom\Docliner\Client\log\adm\Docliner.log
+    + C:\Taxcom\Docliner\Client\log\Пользователь \FormatCheck\FormatCheck.log
+    + C:\Taxcom\Docliner\Server\log\CPCrypto\CPCrypto.log 
+    */
+
+    /*Other
+    + Вся папка C:\Taxcom\Docliner Single\Client\log
+    + Вся папка C:\Taxcom\Docliner Single\Server\log 
+    */
+
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             */
+
+        }
+    }
+}
